db1151303c7d04143ba81e50d7edf74f33bb7cb6
never1068 <jlii0133@student.monash.edu>
2019-08-19 17:33:08 +1000

    “students dictionary

---
+++ FIT5032_week03/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032_week03.Startup))]
+namespace FIT5032_week03
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
